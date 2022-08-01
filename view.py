b9a1ff41f4ba5862fd3e73c2c31164539f59d71f
yizhao2 <40136237+yizhao2@users.noreply.github.com>
2022-08-01 12:18:13 +0800

    Update view.py

--- view.py
+++ view.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2018/6/3 下午5:46
-# @Author  : xianzx
-# @Email   : xianeamil@icloud.com
-# @Site    :
-# @File    : api.py.py
-# @Software: PyCharm
+
 import logging
 import traceback
 
@@ -18,8 +12,6 @@ from config.box import box_manger
 sql_box = box_manger.get('sql')
 
 TAG = 'R'
-
-# 自动增加新接口
 def add_actions(name, url, view):
     try:
         sql = sql_box.get('interface', 'get_interface_by_url')
@@ -27,7 +19,6 @@ def add_actions(name, url, view):
             sql = sql_box.get('interface', 'insert_interface')
             if hasattr(view, 'get'): db_tools.exec_sql(sql, values=(1, name, name, url, 'GET', 1, 0, ''), is_query=False)
             if hasattr(view, 'post'): db_tools.exec_sql(sql, values=(1, name, name, url, 'POST', 1, 0, ''), is_query=False)
-            if hasattr(view, 'put'): db_tools.exec_sql(sql, values=(1, name, name, url, 'PUT', 1, 0, ''), is_query=False)
             if hasattr(view, 'delete'): db_tools.exec_sql(sql, values=(1, name, name, url, 'DELETE', 1, 0, ''), is_query=False)
             logging.warning("database register " + url + " interface.")
         else:
@@ -49,9 +40,6 @@ def register_views(api, views):
     views_class_dict = get_init_py_object(views, TAG, -1)
     for name, view in views_class_dict.items():
         url = '/api' + get_api_url(name)
-        # url1 = '/api/' + name
-        # logging.warning("web register " + url1 + " interface.")
-        logging.warning("web register " + url + " interface.")
         add_actions(name, url, view)
         # add_actions(name, url1, view)
         api.add_resource(view, url)
